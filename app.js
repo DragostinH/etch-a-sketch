2f1330c28ae71072e60c3552d3efe7b4d334175b
DragostinH <dragostin.hristov@gmail.com>
2021-12-25 22:29:00 +0200

    added size buttons and paint funcitonality

--- app.js
+++ app.js
@@ -1,26 +1,62 @@
+// Create the initial grid for when the page loads for the first time
 const mainContainer = document.getElementById('container');
-let test = 0;
-test = mainContainer.style.height;
-console.log(mainContainer.scrollHeight);
+createGrid(mainContainer);
+paintCubes(mainContainer);
 
 
-function createGrid(parentElement, numberOfSquares) {
-    let n = numberOfSquares*numberOfSquares;
-    let containerHeight = parentElement.scrollHeight;
-    let containerWidht = parentElement.scrollWidth;
-    let squareHeight = containerHeight / numberOfSquares;
-    let squareWidth = containerWidht / numberOfSquares;
+let size = "";
+const buttons = document.querySelectorAll('#size-buttons');
+
+buttons.forEach(element => {
+    element.addEventListener('click', e => {
+        size = element.className;
+        removeGrid(mainContainer);
+        createGrid(mainContainer, size);
+        paintCubes(mainContainer);
+    })
+})
+
+
+
+function createGrid(parentElement, numberOfSquares = 4) {
+    let n = numberOfSquares * numberOfSquares; //number for loops
+    let element;
+    let containerHeight = parentElement.scrollHeight; // height of the draw area
+    let containerWidht = parentElement.scrollWidth; // width of the draw area
+    let squareHeight = containerHeight / numberOfSquares; //height of the square based on the container
+    let squareWidth = containerWidht / numberOfSquares; // width of the square based on the container
+
+    // if (element.classList === 'cube-element') {
+    //     parentElement.removeChild('cube-element');
+
+    // }
 
     for (let i = 0; i < n; i++) {
-        let element = document.createElement('div');
+        element = document.createElement('div');
         element.style.height = squareHeight + "px";
         element.style.width = squareWidth + "px";
+        element.classList.add('cube-element');
         // border to view squares
         element.style.border = "1px solid black";
-        // creating the grid
         parentElement.appendChild(element);
     }
-
 }
 
-createGrid(mainContainer, 16);
+function removeGrid(parentElement){
+    while(parentElement.firstChild){
+        parentElement.removeChild(parentElement.firstChild);
+    }
+}
+
+function paintCubes(parentElement){
+    let listOfCubes = parentElement.querySelectorAll('.cube-element');
+    console.log(listOfCubes);
+    listOfCubes.forEach(ele =>{
+        ele.addEventListener("mouseenter", e=>{
+            ele.style.backgroundColor="black";
+        })
+
+    })
+}
+
+
